aeb07720a52e5823d1b76468b7d74028e010736f
Breno Brasil <brenobr.dev@gmail.com>
2022-05-29 19:13:48 -0500

    added Player class status

--- SuperAdventure/SuperAdventure/SuperAdventure.Designer.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelHitPoints = new System.Windows.Forms.Label();
+            this.labelGold = new System.Windows.Forms.Label();
+            this.labelExperience = new System.Windows.Forms.Label();
+            this.labelLevel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hit Points:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gold:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Experience:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Level:";
+            // 
+            // labelHitPoints
+            // 
+            this.labelHitPoints.AutoSize = true;
+            this.labelHitPoints.Location = new System.Drawing.Point(110, 9);
+            this.labelHitPoints.Name = "labelHitPoints";
+            this.labelHitPoints.Size = new System.Drawing.Size(0, 15);
+            this.labelHitPoints.TabIndex = 4;
+            // 
+            // labelGold
+            // 
+            this.labelGold.AutoSize = true;
+            this.labelGold.Location = new System.Drawing.Point(110, 31);
+            this.labelGold.Name = "labelGold";
+            this.labelGold.Size = new System.Drawing.Size(0, 15);
+            this.labelGold.TabIndex = 5;
+            // 
+            // labelExperience
+            // 
+            this.labelExperience.AutoSize = true;
+            this.labelExperience.Location = new System.Drawing.Point(110, 53);
+            this.labelExperience.Name = "labelExperience";
+            this.labelExperience.Size = new System.Drawing.Size(0, 15);
+            this.labelExperience.TabIndex = 6;
+            // 
+            // labelLevel
+            // 
+            this.labelLevel.AutoSize = true;
+            this.labelLevel.Location = new System.Drawing.Point(110, 75);
+            this.labelLevel.Name = "labelLevel";
+            this.labelLevel.Size = new System.Drawing.Size(0, 15);
+            this.labelLevel.TabIndex = 7;
+            // 
+            // SuperAdventure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(719, 651);
+            this.Controls.Add(this.labelLevel);
+            this.Controls.Add(this.labelExperience);
+            this.Controls.Add(this.labelGold);
+            this.Controls.Add(this.labelHitPoints);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SuperAdventure";
+            this.Text = "RPG Adventure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label labelHitPoints;
+        private Label labelGold;
+        private Label labelExperience;
+        private Label labelLevel;
     }
 }
--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -1,10 +1,34 @@
+using System;
+using Engine;
+
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
+        private Player _player;
         public SuperAdventure()
         {
             InitializeComponent();
+            _player = new Player();
+            SetNewPlayer(_player);
+            RefreshScreen();
+        }
+
+        private void SetNewPlayer(Player p)
+        {
+            p.CurrentHitPoints = 10;
+            p.MaximumHitPoints = 10;
+            p.Gold = 0;
+            p.ExperiencePoints = 0;
+            p.Level = 1;
+        }
+
+        private void RefreshScreen()
+        {
+            labelHitPoints.Text = Convert.ToString(_player.CurrentHitPoints);
+            labelGold.Text = Convert.ToString(_player.Gold);
+            labelExperience.Text = Convert.ToString(_player.ExperiencePoints);
+            labelLevel.Text = Convert.ToString(_player.Level);
         }
     }
 }
